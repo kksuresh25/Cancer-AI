c395c8bd5cb06788d3be30203535b67433588200
Krishna Suresh <72838021+kksuresh25@users.noreply.github.com>
2020-12-05 18:29:42 -0500

    Update cancer_ai.sh

--- cancer_ai.sh
+++ cancer_ai.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 #run bash commands
 pip install virtualenv --user
-python -m virtualenv --python=python2.7 venv
-source venv/bin/activate
+python -m venv cancerai_venv
+source cancerai_venv/bin/activate
 pip install -r requirements.txt
 python predict.py
 deactivate
